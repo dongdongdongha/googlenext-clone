1bd81d1070873fd0e8c8c03bd6a44a860bd0308f
류동하 <06dongha@gmail.com>
2024-04-14 17:04:09 +0900

    zz

--- google-next/src/app/page.js
+++ google-next/src/app/page.js
@@ -1,10 +1,18 @@
 import HomeHeader from "@/components/HomeHeader";
-import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+          alt="Google Logo"
+          width={300} 
+          height={100}
+        />
+      </div>
     </>
   );
 }
